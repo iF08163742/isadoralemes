f5996a4aa304a7eb1ead8061aa7035963e9da08a
isadora <161738122+iF08163742@users.noreply.github.com>
2024-05-23 11:57:06 -0300

    dfcg

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivocurso = new Date("2026-12-22T00:00:00");
-const tempoObjetivobros = new Date("2024-09-10T00:00:00");
-const tempoObjetivo3t = new Date("2027-01-01T00:00:00");
-const tempoObjetivoteam = new Date("2028-01-01T00:00:00");
+const tempoObjetivo1 = new Date("2026-12-22T00:00:00");
+const tempoObjetivo2 = new Date("2024-09-10T00:00:00");
+const tempoObjetivo3 = new Date("2027-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2028-01-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
